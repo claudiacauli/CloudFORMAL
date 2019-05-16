acacc8c65edd380ebfc31ff0dc528f7a9029b866
Claudia Cauli <claudiacauli@gmail.com>
2019-05-16 14:22:01 +0200

    Setup CloudFormation node types classes hierarchy

--- src/main/scala/aws/cfn/formalization/StackSet.scala
+++ src/main/scala/aws/cfn/formalization/StackSet.scala
@@ -1,4 +1,52 @@
 package aws.cfn.formalization
+
+
+class StackSet(val name:String, val templates:Vector[Template])
+class Template(val name:String, val inputParameters : Map[String,ValueNode])
+
+trait CloudFormationNode
+
+  trait ValueNode extends CloudFormationNode
+
+    case class StringNode(value: (=> String)) extends ValueNode
+    case class BooleanNode(value: (=> Boolean)) extends ValueNode
+    case class IntNode(value: (=> Int)) extends ValueNode
+    case class FloatNode(value: (=>Float)) extends ValueNode
+    case class LongNode(value: (=>Long)) extends ValueNode
+    case class JsonNode(value: (=>String)) extends ValueNode
+    case class CommaDelimitedLisNode (value: (=>String)) extends ValueNode
+    case class DateTimeNode (value: (=> String)) extends ValueNode
+
+    trait IntrinsicFunction extends ValueNode
+
+      case class Base64Function() extends IntrinsicFunction
+      case class CidrFunction() extends IntrinsicFunction
+      case class FindInMapFunction() extends IntrinsicFunction
+      case class GetAttFunction() extends IntrinsicFunction
+      case class GetAZsFunction() extends IntrinsicFunction
+      case class ImportValueFunction() extends IntrinsicFunction
+      case class JoinFunction() extends IntrinsicFunction
+      case class SelectFunction() extends IntrinsicFunction
+      case class SplitFunction() extends IntrinsicFunction
+      case class SubFunction() extends IntrinsicFunction
+      case class TransformFunction() extends IntrinsicFunction
+      case class RefFunction() extends IntrinsicFunction
+
+      trait ConditionFunction extends IntrinsicFunction
+        case class IfFunction() extends ConditionFunction
+        case class AndFunction() extends ConditionFunction
+        case class OrFunction() extends ConditionFunction
+        case class NotFunction() extends ConditionFunction
+        case class EqualsFunction() extends ConditionFunction
+
+  trait ObjectNode extends CloudFormationNode
+
+    case class ResourceNode() extends ObjectNode
+    case class SubpropertyNode() extends ObjectNode
+    case class PolicyNode() extends ObjectNode
+
+
+
 //
 //sealed trait AttributeValue
 //sealed trait SingleValue
